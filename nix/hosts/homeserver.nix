0a9878171d16124ffdcb1cd1e70b8d22de76d55c
Ed Bruce <ecbruce@proton.me>
2024-10-22 17:39:09 +0100

    Add GRUB and LUKS configs

--- nix/hosts/homeserver.nix
+++ nix/hosts/homeserver.nix
@@ -4,5 +4,21 @@
         ../common/default.nix
     ];
 
+    # Bootloader.
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sde";
+    boot.loader.grub.useOSProber = true;
+
+    boot.initrd.luks.devices."luks-539797c1-fc40-4d19-8861-70867a2b2451".device = "/dev/disk/by-uuid/539797c1-fc40-4d19-8861-70867a2b2451";
+    # Setup keyfile
+    boot.initrd.secrets = {
+        "/crypto_keyfile.bin" = null;
+    };
+
+    boot.loader.grub.enableCryptodisk=true;
+
+    boot.initrd.luks.devices."luks-eccba579-de78-4f4b-801b-22535f7e14fd".keyFile = "/crypto_keyfile.bin";
+    boot.initrd.luks.devices."luks-539797c1-fc40-4d19-8861-70867a2b2451".keyFile = "/crypto_keyfile.bin";
+
     networking.hostName = "homeserver";
 }
