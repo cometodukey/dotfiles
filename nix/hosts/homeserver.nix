049b15ddfcac8d3dd36ff2e4ea661659af97e962
Ed Bruce <ecbruce@proton.me>
2024-10-23 12:33:13 +0100

    Slightly broken server config

--- nix/hosts/homeserver.nix
+++ nix/hosts/homeserver.nix
@@ -1,18 +1,18 @@
+{ config, lib, pkgs, ... }:
+
 {
     imports = [
         ../hardware/homeserver.nix
         ../common/default.nix
     ];
 
-    # Bootloader
-    # TODO switch to grub
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-
-    # Hostname
-    networking.hostName = "homeserver";
-
-    defaults.sshd = enable;
+    config = {
+        boot.loader.systemd-boot.enable = true;
+        boot.loader.efi.canTouchEfiVariables = true;
+        networking.hostname = "homeserver";
+        custom.sshd = true;
+        system.stateVersion = "24.05";
+    };
 
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
@@ -20,5 +20,5 @@
     # this value at the release version of the first install of this system.
     # Before changing this value read the documentation for this option
     # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "24.05";# Did you read the comment?
+    # system.stateVersion = "24.05";# Did you read the comment?
 }
