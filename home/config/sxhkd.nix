8ce1271afb7b9d661a83540372fe8f4c27afce97
Ed Bruce <ecbruce@proton.me>
2024-10-29 17:13:41 +0000

    More complete sxhkd config

--- home/config/sxhkd.nix
+++ home/config/sxhkd.nix
@@ -1,5 +1,8 @@
-{ config, inputs, lib, options, system, ... }:
+{ config, inputs, lib, options, pkgs, system, ... }:
 
+let
+    spectacle = "${pkgs.spectacle}/bin/spectacle";
+in
 {
     services.sxhkd = {
         enable = true;
@@ -8,9 +11,11 @@
             # Program bindings
             "super + Return" = "${config.programs.alacritty.package}/bin/alacritty";
             "super + b" = "${config.programs.firefox.package}/bin/firefox";
-            # "super + e" = editor;
+            # "super + e" = "$TERM -e $EDITOR";
             # "super + r" = program launcher; 
-            # "Print" = screenshot program;
+            "Print" = "${spectacle} -fcb"; # Fullscreen screenshot
+            "shift + Print" = "${spectacle} -rcb"; # Selected region screenshot
+            "shift + alt + Print" = "${spectacle} -acb"; # Selected window screenshot
 
             # Audio keys
             "{XF86AudioRaiseVolume,XF86AudioLowerVolume}" = "pactl set-sink-volume @DEFAULT_SINK@ {+,-}5%";
@@ -21,14 +26,26 @@
             "XF86MonBrightness{Up,Down}" = "brightnessctl set 5%{+,-}";
 
             # Reloading
-            "super + alt + {q,r}" = "bspc {quit,wm-r}";
-            "super + alt + Escape" = "pkill -USR1 -x sxkhd";
-            # quit/reload polybar
+            "super + alt + {q,r}" = "bspc {quit,wm -r}";
+            "super + alt + Escape" = "pkill -USR1 sxkhd";
+            "super + alt + p" = "systemctl --user restart polybar.service";
             # quit/reload program launcher
 
-            # Window management
-            "super + q" = "bspc node -c -k";
-            # Move windows, send to workspace, resize, floating, tiling, etc
+            # focus/swap
+
+            # focus or send to the given desktop
+            "super + {_,shift + }{1-9,0}" = "bspc {desktop -f,node -d} '^{1-9,10}'";
+
+            # focus the node in the given direction
+            "super + {_,shift + } {h,j,k,l}" = "bspc node -{f,s} {west,south,north,east}";
+
+           # move/resize
+
+           # expand a window by moving one of its side outward
+           "super + alt + {h,j,k,l}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
+
+          # contract a window by moving one of its side inward
+          "super + alt + shift + {h,j,k,l}" = "bspc node -z {right -20 0,top 0 20,bottom 0 -20,left 20 0}";
         };
     };
 }
