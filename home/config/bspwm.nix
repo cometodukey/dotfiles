d10e8c891b173a8fc32a1aa9b177584f97826d11
Ed Bruce <ecbruce@proton.me>
2024-10-29 09:40:18 +0000

    Reformat bspwm config

--- home/config/bspwm.nix
+++ home/config/bspwm.nix
@@ -1,5 +1,5 @@
-# The bspwm window manager home-manager module
 { lib, config, pkgs, ... }:
+
 {
     xsession = {
         enable = true;
@@ -7,27 +7,28 @@
 
     home.packages = [ config.xsession.windowManager.bspwm.package ];
 
-  xdg = {
-    enable = true;
-    configFile = {
-      "bspwm/bspwmrc" = {
+    xdg = {
         enable = true;
-        executable = true;
-        onChange = "bspc wm -r";
-        text = ''
-          #!/bin/sh
-          mainmonitor="$(bspc query --monitors --names | head -n 1)"
-          bspc monitor $mainmonitor \^1 -d I II III IV V VI VII VIII IX X
-          bspc config border_width         0
-          bspc config window_gap           10
-          bspc config split_ratio          0.52
-          bspc config borderless_monocle   true
-          bspc config gapless_monocle      true
-          bspc rule -a Emacs state=tiled
-        '';
-      };
+        configFile = {
+          "bspwm/bspwmrc" = {
+            enable = true;
+            executable = true;
+            onChange = "bspc wm -r";
+            text = ''
+              #!/bin/sh
+              mainmonitor="$(bspc query --monitors --names | head -n 1)"
+              bspc monitor $mainmonitor -d 1 2 3 4 5 6 7 8 9 10
+              bspc desktop -f 1
+              bspc config border_width         0
+              bspc config window_gap           10
+              bspc config split_ratio          0.52
+              bspc config borderless_monocle   true
+              bspc config gapless_monocle      true
+              bspc rule -r "*"
+            '';
+          };
+        };
     };
-  };
 }
 
 
